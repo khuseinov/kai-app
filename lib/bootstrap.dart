4aa149bfd5be445be11365e81018f0a86780cf01
khuseinov <nirro-99@mail.ru>
2026-04-09 23:19:38 +0300

    feat: setup ProviderScope and async bootstrap

---
+++ lib/bootstrap.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'core/network/connectivity_service.dart';
+
+Future<ProviderContainer> bootstrap() async {
+  await Hive.initFlutter();
+  await Future.wait([
+    Hive.openBox('settings'),
+    Hive.openBox('chat_history'),
+    Hive.openBox('sessions'),
+    Hive.openBox('pending_messages'),
+  ]);
+
+  final container = ProviderContainer();
+  
+  final connectivityService = container.read(connectivityServiceProvider);
+  await connectivityService.init();
+  
+  return container;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-
+import 'bootstrap.dart';
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
-
-  // Pre-register adapters before opening boxes
-  await Hive.openBox('settings');
-  await Hive.openBox('chat_history');
-
-  runApp(const ProviderScope(child: KaiApp()));
+  
+  final container = await bootstrap();
+  
+  runApp(
+    UncontrolledProviderScope(
+      container: container,
+      child: const KaiApp(),
+    ),
+  );
 }
