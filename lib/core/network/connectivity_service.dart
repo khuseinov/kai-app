bc1a3fe28f1f86e3cb166cb30657b02a0d69bb55
khuseinov <nirro-99@mail.ru>
2026-04-09 23:14:59 +0300

    feat: add ConnectivityService and provider

---
+++ lib/core/network/connectivity_service.dart
@@ -0,0 +1,27 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ConnectivityService {
+  final Connectivity _connectivity;
+  bool _isOnline = true;
+
+  ConnectivityService(this._connectivity) {
+    _connectivity.onConnectivityChanged.listen((List<ConnectivityResult> results) {
+      _isOnline = !results.contains(ConnectivityResult.none);
+    });
+  }
+
+  bool get isOnline => _isOnline;
+  
+  Stream<bool> get onConnectivityChanged => _connectivity.onConnectivityChanged
+      .map((results) => !results.contains(ConnectivityResult.none));
+
+  Future<void> init() async {
+    final results = await _connectivity.checkConnectivity();
+    _isOnline = !results.contains(ConnectivityResult.none);
+  }
+}
+
+final connectivityServiceProvider = Provider<ConnectivityService>((ref) {
+  return ConnectivityService(Connectivity());
+});
